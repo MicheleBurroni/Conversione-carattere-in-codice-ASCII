3e1873f98d4909760fa3b02f31c6d16e18748d26
michiburro14@gmail.com <michiburro14@gmail.com>
2020-11-10 21:29:58 +0100

    Add project files.

---
+++ ConversioneCarattere/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConversioneCarattere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserisci una lettera di cui vuoi sapere in numero in ASCII");
+            char car = Convert.ToChar(Console.ReadLine());
+            ushort carASCII = 0;
+            carASCII = car;
+            string n = Convert.ToString(carASCII, 2).PadLeft(16, '0');
+            Console.WriteLine($"Carattere {car} - Carattere in ASCII {carASCII} - NumeroBinario {n}");
+
+        }
+    }
+    
+}
